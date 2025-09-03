722f1de0de43503dcb1537f93599e43730d71872
Keerthik <keerthikshetty2@example.com>
2025-09-04 03:10:41 +0530

    deploy-1a(frontend)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "https://nfc4-the-codefather-1.onrender.com",
         changeOrigin: true,
       },
     },
